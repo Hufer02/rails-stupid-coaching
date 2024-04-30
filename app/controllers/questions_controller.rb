9d9a630e40f64e553e681112b5068824c6480cdc
Hugo Ferrier <hu.ferrier@gmail.com>
2024-04-30 21:00:43 +0200

    did the exercise

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,23 @@
+class QuestionsController < ApplicationController
+  def ask
+    @ask = params[:ask]
+  end
+
+  def answer
+    if params[:ask] == "I am going to work"
+      @answers = "Great !"
+    elsif params[:ask] == "?"
+      @answers = "Silly question, get dressed and go to work!"
+    else
+      @answers = "I don't care, get dressed and go to work!"
+    end
+  end
+end
+
+# if your_message == STOP_MESSAGE || your_message == STOP_MESSAGE.upcase
+#   ""
+# elsif your_message.end_with?("?")
+#   "Silly question, get dressed and go to work!"
+# else
+#   "I don't care, get dressed and go to work!"
+# end
